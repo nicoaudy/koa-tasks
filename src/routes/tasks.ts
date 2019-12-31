b36ec779346325bd457dd48446c949464f71cb73
Nico Audy <nicoaudy1994@gmail.com>
2019-12-31 10:24:58 +0700

    create the tasks route

---
+++ src/routes/tasks.ts
@@ -0,0 +1,26 @@
+import * as Router from 'koa-router';
+
+const router = new Router();
+
+router.get('/tasks', async(ctx) => {
+	ctx.body = "Return all tasks";
+});
+
+router.get('/tasks/:id', async(ctx) => {
+	const param = ctx.params.id;
+	ctx.body = "Return tasks with id" + param;
+});
+
+router.post('/tasks', async(ctx) => {
+	ctx.body = "Create a new tasks";
+});
+
+router.put('/tasks/:id', async(ctx) => {
+	const param = ctx.params.id;
+	ctx.body = "Update tasks with id " + param;
+});
+
+router.delete('/tasks/:id', async(ctx) => {
+	const param = ctx.params.id;
+	ctx.body = "Delete tasks with id " + param;
+});
