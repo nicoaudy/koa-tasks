121b713d2ef7e873dedbe6f36a0938b1203fc6c7
Nico Audy <nicoaudy1994@gmail.com>
2019-12-31 10:46:08 +0700

    task repository done

--- src/repositories/inMemoryTaskRepository.ts
+++ src/repositories/inMemoryTaskRepository.ts
@@ -4,24 +4,38 @@ import { Task } from '../models/task';
 export class InMemoryTaskRepository implements InterfaceTaskRepository {
 
 	private _tasks: Task[] = [];
+	private _nextId = 1;
 
-	getAll(): Promise<Task[]> {
-
+	async getAll(): Promise<Task[]> {
+		return this._tasks;
 	}
 
-	get(taskId: number): Promise<Task> {
-
+	async get(taskId: number): Promise<Task> {
+		const task = this._tasks.find(t => t.id === taskId);
+		return task;
 	}
 
-	create(task: Task): Promise<Task> {
+	async create(task: Task): Promise<Task> {
+		task.id = this._nextId;
+		this._nextId++;
+		this._tasks.push(task);
 
+		return task;
 	}
 
-	update(taskId: number, task: Task): Promise<Task> {
+	async update(taskId: number, task: Task): Promise<Task> {
+		let updateTask = this._tasks.filter(t => t.id === taskId)[0];
+		updateTask.name = task.name;
+		updateTask.isComplete = task.isComplete;
 
+		return task;
 	}
 
-	delete(taskId: number): Promise<Task> {
+	async delete(taskId: number): Promise<Task> {
+		const index = this._tasks.findIndex(t => t.id === taskId);
+		const task = this._tasks[index];
+		this._tasks.splice(index, 1);
 
+		return task;
 	}
 }
