09054440555f6e25798d97883320d183f8a2e83b
Nico Audy <nicoaudy1994@gmail.com>
2019-12-31 10:36:38 +0700

    done with repository interface

--- src/repositories/taskRepository.ts
+++ src/repositories/taskRepository.ts
@@ -2,4 +2,8 @@ import { Task } from '../models/task';
 
 export interface InterfaceTaskRepository {
 	getAll(): Promise<Task[]>;
+	get(taskId: number): Promise<Task>;
+	create(task: Task): Promise<Task>;
+	update(taskId: number, task: Task): Promise<Task>;
+	delete(taskId: number): Promise<Task>;
 }
